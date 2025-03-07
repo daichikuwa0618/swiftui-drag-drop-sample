01152f33471a7766ecce2800d3f9c7a46a1e0bbb
Daichi Hayashi <develop@daichidaiji.com>
2025-03-07 12:37:06 +0900

    feat: initialize project

---
+++ DragDropSample/DragDropSample/DragDropSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  DragDropSampleApp.swift
+//  DragDropSample
+//
+//  Created by Daichi Hayashi on 2025/03/07.
+//
+
+import SwiftUI
+
+@main
+struct DragDropSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DragDropSample/DragDropSampleTests/DragDropSampleTests.swift
@@ -0,0 +1,17 @@
+//
+//  DragDropSampleTests.swift
+//  DragDropSampleTests
+//
+//  Created by Daichi Hayashi on 2025/03/07.
+//
+
+import Testing
+@testable import DragDropSample
+
+struct DragDropSampleTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
