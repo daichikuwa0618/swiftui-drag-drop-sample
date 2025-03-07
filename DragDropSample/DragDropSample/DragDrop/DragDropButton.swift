1c76a1dfc873758ec7b065ca5c9973b419221ac5
Daichi Hayashi <develop@daichidaiji.com>
2025-03-07 12:43:53 +0900

    feat: add button

---
+++ DragDropSample/DragDropSample/DragDrop/DragDropButton.swift
@@ -0,0 +1,28 @@
+//
+//  DragDropButton.swift
+//  DragDropSample
+//
+//  Created by Daichi Hayashi on 2025/03/07.
+//
+
+import SwiftUI
+
+struct DragDropButton: View {
+  let title: String
+
+  private let cornerRadius: CGFloat = 8
+
+  var body: some View {
+    Text(title)
+      .padding()
+      .background {
+        RoundedRectangle(cornerRadius: cornerRadius)
+          .stroke(Color.gray.opacity(0.5), lineWidth: 2)
+          .foregroundStyle(Color.gray.opacity(0.2))
+      }
+  }
+}
+
+#Preview {
+  DragDropButton(title: "私は")
+}
